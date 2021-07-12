4aeadb90f97ccbc33b95156a6ed571d2817c9743
XayEss <alex.seljuk@gmail.com>
2021-07-12 18:25:11 +0300

    pomXMLParser Homework

---
+++ xmlParser/Main.java
@@ -0,0 +1,28 @@
+package xmlParser;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		PomParser parser = new PomParser();
+		try {
+			System.out.println(parser.parsePom());
+		} catch (ParserConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
---
+++ xmlParser/Manifest.java
@@ -0,0 +1,30 @@
+package xmlParser;
+
+public class Manifest {
+	private boolean addClasspath;
+	private String mainClass;
+	
+	public boolean isAddClasspath() {
+		return addClasspath;
+	}
+	public void setAddClasspath(boolean addClasspath) {
+		this.addClasspath = addClasspath;
+	}
+	public String getMainClass() {
+		return mainClass;
+	}
+	public void setMainClass(String mainClass) {
+		this.mainClass = mainClass;
+	}
+	
+	public static Manifest newInstance() {
+		return new Manifest();
+	}
+	@Override
+	public String toString() {
+		return "Manifest [addClasspath=" + addClasspath + ", mainClass=" + mainClass + "]";
+	}
+	
+	
+
+}
